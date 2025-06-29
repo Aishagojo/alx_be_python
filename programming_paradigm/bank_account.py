790a791eeb7619e76fa6628ac6b99d8faeedc3aa
Aishagojo <gojoaisha@gmail.com>
2025-06-29 02:39:12 -0700

    fixed bank_accoun.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -9,8 +9,9 @@ class BankAccount:
         if amount <= self.account_balance:
             self.account_balance -= amount
             return True
-        return False
+        else:
+            return False
 
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
 
